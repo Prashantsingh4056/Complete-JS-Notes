80d2b4c982d5724c32698e756fd190b5b1c72ccb
Prashantsingh4056 <prashantsingh97393535@gmail.com>
2026-05-25 06:48:32 +0000

    learned about Convertion Operations

--- JS-Code-Implementation/01_basics/03_conversionOperations.js
+++ JS-Code-Implementation/01_basics/03_conversionOperations.js
@@ -35,6 +35,40 @@ console.log(stringNumber);  // 33
 console.log(typeof stringNumber);  // string
 
 
+// *********************************Operations**********************************
+
+let value  = 3
+let negValue = -value
+console.log(negValue); // -3
+
+let str1 = "hello"
+let str2 = " Prashant"
+
+let str3 = str1 + str2
+console.log(str3);  // hello Prashant
+
+console.log("1" + 2); // 12
+console.log(1 + "2");  // 12
+console.log("1" + 2 + 2);  // 122
+console.log(1 + 2 + "2");  // 32
+
+console.log(true);  // true
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2  // (less readable)
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
 
 
 
